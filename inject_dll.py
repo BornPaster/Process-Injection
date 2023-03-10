a37a556db96956d2316d972c35b8b328fbebfbf6
Zappy <126213174+BornPaster@users.noreply.github.com>
2023-03-10 01:11:54 -0600

    Add files via upload

---
+++ inject_dll.py
@@ -0,0 +1,80 @@
+import ctypes
+import sys
+
+# Define the DLL code to be injected
+dll_code = """
+#include <Python.h>
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
+{
+    if (ul_reason_for_call == DLL_PROCESS_ATTACH)
+    {
+        Py_Initialize();
+        PyRun_SimpleString("print('success')");
+        Py_Finalize();
+    }
+    return TRUE;
+}
+"""
+
+# Compile the code into a DLL file
+import subprocess
+import os
+import platform
+
+dll_file = "success.dll"
+if platform.system() == "Windows":
+    gcc_command = "gcc -shared -o {} -xc -".format(dll_file)
+elif platform.system() == "Linux":
+    gcc_command = "gcc -shared -o {} -x c -".format(dll_file)
+else:
+    raise OSError("Unsupported platform")
+
+subprocess.run(gcc_command, input=dll_code.encode(), check=True, shell=True)
+
+# Get the process ID from the command line argument
+if len(sys.argv) < 2:
+    print("Usage: python inject_dll.py <pid>")
+    sys.exit(1)
+pid = int(sys.argv[1])
+
+# Open the target process
+process_handle = ctypes.windll.kernel32.OpenProcess(
+    ctypes.c_uint32(0x1F0FFF),  # All access
+    ctypes.c_bool(False),
+    ctypes.c_uint32(pid)
+)
+
+# Allocate memory for the DLL path in the target process
+dll_path = ctypes.c_char_p(os.path.abspath(dll_file).encode("utf-8"))
+dll_path_size = ctypes.c_size_t(len(dll_path.value) + 1)
+remote_dll_path = ctypes.windll.kernel32.VirtualAllocEx(
+    process_handle,
+    None,
+    dll_path_size.value,
+    ctypes.c_uint32(0x3000),  # Commit, read, write
+    ctypes.c_uint32(0x04)  # Read, write
+)
+
+# Write the DLL path to the target process memory
+ctypes.windll.kernel32.WriteProcessMemory(
+    process_handle,
+    remote_dll_path,
+    dll_path,
+    dll_path_size,
+    None
+)
+
+# Load the DLL in the target process
+thread_id = ctypes.windll.kernel32.CreateRemoteThread(
+    process_handle,
+    None,
+    0,
+    ctypes.windll.kernel32.GetProcAddress(ctypes.windll.kernel32.GetModuleHandleA("kernel32.dll"), "LoadLibraryA"),
+    remote_dll_path,
+    0,
+    None
+)
+
+# Wait for the thread to exit
+ctypes.windll.kernel32.WaitForSingleObject(thread_id, -1)
